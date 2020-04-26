87aac14023e5db575654951943b6e7ef9c3b60b6
Confrizz <belous.philip@gmail.com>
2020-04-25 19:50:02 -0600

    Initial commit

---
+++ src/main/java/com/github/confrizz/basemod/BaseMod.java
@@ -0,0 +1,13 @@
+package com.github.confrizz.basemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BaseMod.MODID)
+public class BaseMod {
+    public static final String MODID = "basemod";
+
+    public BaseMod () {
+
+    }
+
+}
